d05ad0300bfa6f52cb104effbf5c7aaf866cdc6b
girliansantos <girliansantos@si.fiponline.edu.br>
2021-11-11 10:47:17 -0200

    Initial Commit - Project with Spring

---
+++ checked/src/test/java/com/codeline/tec/checked/CheckedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeline.tec.checked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
